83d8bc84bcf7491b0669857eefdeb909d994abf3
kwanj-k <mwangikelvin23@.com>
2018-11-27 16:57:15 +0300

    Add project setup and crud operations for products The views are implemented with regular django for now

---
+++ storemanager/products/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Product(models.Model):
+    name        = models.CharField(max_length=100)
+    category    = models.CharField(max_length=50)
+    inventory   = models.IntegerField()
+    price       = models.IntegerField()
+    timestamp	= models.DateTimeField(auto_now_add=True)
+    updated		= models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering =['-updated', '-timestamp']
+
+    def __str__(self):
+        return self.name
+
+    objects = models.Manager()
