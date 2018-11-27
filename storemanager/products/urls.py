f9e4c79b7a342183d38e470ce97fe844c3cc7676
kwanj-k <mwangikelvin23@.com>
2018-11-27 19:31:12 +0300

    Add generic views

--- storemanager/products/urls.py
+++ storemanager/products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from products import views
 
 urlpatterns = [
-    path('products/', views.product_list),
-    path('products/<int:pk>/',views.product_detail)
+    path('products/', views.ProductList.as_view()),
+    path('products/<int:pk>/',views.ProductDetail.as_view())
 ]
--- storemanager/products/views.py
+++ storemanager/products/views.py
@@ -1,49 +1,11 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
-@api_view(['GET', 'POST'])
-def product_list(request):
-    """
-    List all products, or create a new product.
-    """
-    if request.method == 'GET':
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def product_detail(request, pk):
-    """
-    Retrieve, update or delete a product.
-    """
-    try:
-        product = Product.objects.get(pk=pk)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-
-
-    if request.method == 'PUT':
-        serializer = ProductSerializer(product, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
