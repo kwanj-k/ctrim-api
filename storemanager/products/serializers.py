e190fd20830332160ecf82504c42bd70d0d7ade6
kwanj-k <mwangikelvin23@.com>
2018-11-28 10:06:54 +0300

    Add basic test

--- storemanager/products/serializers.py
+++ storemanager/products/serializers.py
@@ -6,9 +6,3 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('id', 'name', 'category', 'inventory', 'price')
-    
-    # id          = serializers.IntegerField(read_only=True)
-    # name        = serializers.CharField(required=True, allow_blank=False, max_length=100)
-    # category    = serializers.CharField(required=False, allow_blank=True, max_length=50)
-    # inventory   = serializers.IntegerField(required=True,all_blank= False)
-    # price       = serializers.IntegerField(required=True,all_blank= False)
--- storemanager/products/urls.py
+++ storemanager/products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from products import views
 
 urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/',views.ProductDetail.as_view())
+    path('products/', views.ProductList.as_view(),name='list'),
+    path('products/<int:pk>/',views.ProductDetail.as_view(),name='details')
 ]
