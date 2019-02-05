4959caa3488729b84070684276f2d6c087bd99b7
kwanj-k <mwangikelvin23@gmail.com>
2019-02-05 13:13:45 +0300

    Fix social auth

--- storemanager/storemanager/urls.py
+++ storemanager/storemanager/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
     path('', include('users.urls')),
+    path('auth/', include('rest_framework_social_oauth2.urls')),
 ]
