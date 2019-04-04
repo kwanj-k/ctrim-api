b0926a3073448dfdfd3bb8a0d2732f8446045e35
kwanj-k <mwangikelvin23@gmail.com>
2019-04-04 19:50:01 +0300

    Fix swagger

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
+
+# set the title for the API.
+schema_view = get_swagger_view(title="Ctrim API")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('products.urls')),
     path('api/', include('users.urls')),
     path('users/login',include('rest_framework.urls')),
+    path('', schema_view),
 ]
