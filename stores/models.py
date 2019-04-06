719deac94c772d672350e0c88fbfc6710df0bbb8
Kelvin Mwangi <33450849+kwanj-k@users.noreply.github.com>
2019-04-06 13:30:57 +0300

    Stores (#15)
    * Morphed into ctrim
    
    * Add procfile
    
    * Fix settings
    
    * Fix migrations
    
    * Fix swagger
    
    * Fix swagger
    
    * Fix login
    
    * Add staff
    
    * Fix pass hashing
    
    * Fix stores view

--- stores/models.py
+++ stores/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.auth.base_user import  BaseUserManager
+from django.contrib.auth.hashers import make_password
 
 from users.models import User
 from common.models import AbstractBase
 
+
+class StaffManager(BaseUserManager):
+
+    def create_staff(self, username, email, store_id,password=None):
+        phash = make_password(password)
+        staff = self.model(
+            username=username,
+            email=email,
+            store_id=store_id,
+            password=phash
+        )
+        staff.save()
+        return staff
 
 class Store(AbstractBase):
     owner = models.ForeignKey(
@@ -27,3 +42,5 @@ class Staff(User):
         on_delete=models.CASCADE,
         null=False
     )
+
+    objects =  StaffManager()
--- stores/serializers.py
+++ stores/serializers.py
@@ -3,6 +3,8 @@ from rest_framework.validators import UniqueValidator
 
 from .models import Store, Staff
 from users.serializers import UserSerializer
+from users.models import User
+
 
 class StoreSerializer(serializers.ModelSerializer):
     owner = serializers.SerializerMethodField()
@@ -19,10 +21,16 @@ class StoreSerializer(serializers.ModelSerializer):
         )
 
 class StaffSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=128, write_only=True)
 
     class Meta:
         model = Staff
         fields = (
             'username',
             'email',
+            'password',
         )
+
+    def create(self, validated_data):
+        #import pdb; pdb.set_trace()
+        return Staff.objects.create_staff(**validated_data)
