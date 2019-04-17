d885c99341ffe4054d4251b63ebc3f4552247049
kwanj-k <mwangikelvin23@gmail.com>
2019-04-18 01:01:30 +0300

    Get store by id

---
+++ helpers/store.py
@@ -0,0 +1,30 @@
+"""
+File contains all store related helper
+functions
+"""
+from stores.models import Store
+
+def user_stores(request):
+    """
+    Get stores related to a particular user
+    """
+    user = request.user
+    queryset = Store.objects.filter(
+        owner=user
+    )
+    if len(queryset) < 1:
+        all_stores = Store.objects.all()
+        for i in all_stores:
+            staff = Staff.objects.filter(
+                store=i.pk
+            ).first()
+            try:
+                user.username == staff.username
+                store = staff.store
+                queryset = Store.objects.filter(
+                    pk=store.pk
+                )
+            except:
+                continue
+    return queryset
+
