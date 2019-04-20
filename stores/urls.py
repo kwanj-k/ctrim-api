a477cd0cd979d4e96bc5308392ab6c1bb9e3dec0
Kelvin Mwangi <33450849+kwanj-k@users.noreply.github.com>
2019-04-20 22:43:28 +0300

    store detail
    * Morphed into ctrim
    
    * Add procfile
    
    * Fix settings
    
    * Fix migrations
    
    * Fix swagger
    
    * Fix swagger
    
    * Fix login
    
    * Add staff
    
    * Fix pass hashing
    
    * Fix stores view
    
    * Return pk when getting stores
    
    * Fix dependancy issue
    
    * Get store by id

--- stores/urls.py
+++ stores/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from .views import StoreListCreateView, StaffListCreateView
+from .views import (
+    StoreListCreateView,
+    StaffListCreateView,
+    GetUpdateDestroyStoreView
+)
 
 app_name = 'store'
 
 urlpatterns = [
     path('stores/', StoreListCreateView.as_view()),
+    path('stores/<int:pk>/', GetUpdateDestroyStoreView.as_view()),
     path('<slug>/staff/', StaffListCreateView.as_view()),
 ]
