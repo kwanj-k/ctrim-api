7d692dfe61c54903efff6eeb5ca47da39df1f167
kwanj-k <mwangikelvin23@gmail.com>
2019-06-22 18:58:06 +0300

    Fix get stores endpoint

--- apps/helpers/store.py
+++ apps/helpers/store.py
@@ -12,18 +12,18 @@ def user_stores(request):
     queryset = Store.objects.filter(
         owner=user
     )
-    if len(queryset) < 1:
-        all_stores = Store.objects.all()
-        for i in all_stores:
-            staff = Staff.objects.filter(
-                store=i.pk
-            ).first()
-            try:
-                user.username == staff.username
-                store = staff.store
-                queryset = Store.objects.filter(
-                    pk=store.pk
-                )
-            except:
-                continue
+    # if len(queryset) < 1:
+    #     all_stores = Store.objects.all()
+    #     for i in all_stores:
+    #         try:
+    #             staff = Staff.objects.filter(
+    #             store=i.pk
+    #             ).first()
+    #             user.username == staff.username
+    #             store = staff.store
+    #             queryset = Store.objects.filter(
+    #                 pk=store.pk
+    #             )
+    #         except:
+    #             continue
     return queryset
